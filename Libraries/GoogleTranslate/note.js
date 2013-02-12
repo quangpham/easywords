40e709ef7898d3ba96b979ce2a458cc7febda68d
Quang Pham <me@quangpham.com>
2013-02-12 18:09:16 +0200

    .

--- Libraries/GoogleTranslate/note.js
+++ Libraries/GoogleTranslate/note.js
@@ -2,9 +2,11 @@ function tranlateRequest(_keyword, _from, _to)
 {
 	var xmlHttp = null;
 	xmlHttp = new XMLHttpRequest();
-	xmlHttp.open( "GET", "http://translate.google.com/translate_a/t?client=webapp&hl=en&sc=1&q=" + _keyword + "&sl=" + _from + "&tl=" + _to , false );
+	xmlHttp.open( 'GET', 'http://translate.google.com/translate_a/t?client=webapp&hl=en&sc=1&q=' + _keyword + '&sl=' + _from + '&tl=' + _to , false );
 	xmlHttp.send( null );
 	return xmlHttp.responseText;
 }
 
-tranlateRequest("moi","fi","en");
+tranlateRequest('moi','fi','en');
+
+https://www.google.com/search?q=koulu&um=1&ie=UTF-8&hl=en&tbm=isch
